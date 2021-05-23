21f81d6096f50927e332a224e402d09116009308
jukka.hell <jukka.hell@gmail.com>
2021-05-23 16:03:47 +0300

    Comet movement

---
+++ Assets/Scripts/CometUpdater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CometUpdater : MonoBehaviour
+{
+    public Vector2 direction;
+    public float moveSpeed = 20.0f;
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        body.AddForce(direction * moveSpeed);
+    }
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/DeployComet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeployComet : MonoBehaviour
+{
+    public GameObject cometPrefab;
+    public Transform player;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+        StartCoroutine(CometWave());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnComet() {
+        GameObject c = Instantiate(cometPrefab) as GameObject;
+        c.transform.position = player.position + player.TransformDirection(new Vector2(screenBounds.x * 1.5f, screenBounds.y * 1f));
+        c.transform.eulerAngles = player.eulerAngles;
+
+        CometUpdater cometUpdater = c.GetComponent(typeof(CometUpdater)) as CometUpdater;
+        Vector2 dir = c.transform.TransformDirection(new Vector2(-0.5f, -0.5f));
+        cometUpdater.direction = dir.normalized;
+    }
+
+    IEnumerator CometWave() {
+        while(true) {
+            yield return new WaitForSeconds(Random.Range(5f, 10f));
+            SpawnComet();
+        }
+    }
+}
