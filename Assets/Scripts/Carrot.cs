207d0df8d9d0d9af902d956a3fd07403ad1e4c98
jukka.hell <jukka.hell@gmail.com>
2021-05-24 03:07:12 +0300

    Additions Add carrot pickup Change planet Add some post processing Increase comet mass

---
+++ Assets/Scripts/Carrot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrot : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            PickUp();
+        }
+    }
+
+    private void PickUp() {
+        Debug.Log("Power up jea");
+        Destroy(gameObject);
+    }
+}
