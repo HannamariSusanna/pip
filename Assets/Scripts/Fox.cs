405a2f2fdb8ab8c55d559ced0198ddce1442cc9e
jukka.hell <jukka.hell@gmail.com>
2021-05-25 04:32:52 +0300

    Fox chasing logic

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    public Transform player;
+    public Rigidbody2D body;
+
+    public float moveSpeed = 20f;
+
+    private bool isChasing = false;
+    private Vector3 spawnPos;
+    private bool hasSpawned = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (hasSpawned) {
+            if (isChasing) {
+                Vector3 targetDir = player.position - transform.position;
+                body.AddForce(targetDir.normalized * moveSpeed);
+            } else {
+                Vector3 targetDir = spawnPos - transform.position;
+                body.AddForce(targetDir.normalized * moveSpeed);
+            }
+        }
+    }
+
+    public void Spawn(Vector3 spawnPoint) {
+        if (!hasSpawned) {
+            spawnPos = new Vector3(spawnPoint.x, spawnPoint.y, 0f);
+            transform.position = new Vector2(spawnPos.x, spawnPos.y);
+            hasSpawned = true;
+        }
+        isChasing = true;
+    }
+
+    public void FallBack() {
+        isChasing = false;
+    }
+}
---
+++ Assets/Scripts/FoxTerritory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxTerritory : MonoBehaviour
+{
+    public GameObject fox;
+    public Vector3 spawnPoint;
+
+    private Fox foxScript;
+
+    public void Start() {
+        foxScript = fox.GetComponent(typeof(Fox)) as Fox;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            foxScript.Spawn(transform.TransformPoint(spawnPoint));
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            foxScript.FallBack();
+        }
+    }
+}
