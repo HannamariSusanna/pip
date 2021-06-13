695c6404e4d24e866be3e4370bb5ed96f90a7058
jukka.hell <jukka.hell@gmail.com>
2021-06-13 14:30:15 +0300

    Clean up

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -8,13 +8,13 @@ public class HealthBar : MonoBehaviour
     public Slider slider;
     public Image fill;
 
-    public void SetMaxHealth(int energy) {
-        slider.maxValue = energy;
-        slider.value = energy;
+    public void SetMaxHealth(int health) {
+        slider.maxValue = health;
+        slider.value = health;
     }
 
-    public void SetHealth(int energy) {
-        slider.value = energy;
+    public void SetHealth(int health) {
+        slider.value = health;
     }
 
     public void Disabled() {
